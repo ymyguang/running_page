080b5cda568e7ba18810d7515907598a2cd6a99a
Ginicke_kd <34205471+ymyguang@users.noreply.github.com>
2023-04-09 21:32:51 +0800

    Update const.js

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoiaHl0aWNlbiIsImEiOiJjbGc5ZzI1a3YwMzYxM2RxbGRuNmRsYXI1In0.MqVtHSd_Ke8DDpLCkDM7Iw';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
